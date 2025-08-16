57abf3038cbfbbc91e38d8ea921fa2b3d92472e1
SachinSharma22 <s830sharma@gmail.com>
2025-08-17 00:52:59 +0530

    initial commit

---
+++ 10SimpleProblems/problem10.ts
@@ -0,0 +1,4 @@
+// 10. Async / Promise with Types
+
+// Write an async function fetchData(url: string): Promise<string> that simulates fetching data (use setTimeout to return "Data from ${url}").
+// Call the function and log the result.
--- 10SimpleProblems/problem3.ts
+++ 10SimpleProblems/problem3.ts
@@ -13,8 +13,3 @@ const a: Point = { x: 3, y: 2 };
 const b: Point = { x: 4, y: 3 };
 
 console.log(distance(a, b));
-
-// 10. Async / Promise with Types
-
-// Write an async function fetchData(url: string): Promise<string> that simulates fetching data (use setTimeout to return "Data from ${url}").
-// Call the function and log the result.

466b839d4f9b2ae734a203cdb3c6d8a8eccdc366
SachinSharma22 <s830sharma@gmail.com>
2025-08-18 16:30:37 +0530

    use the destructuring property and also pass touple as parameter

--- Basics/touple.js
+++ Basics/touple.js
@@ -1,18 +1,27 @@
-var arr_Tuple = [23, "sachin", "sharma", 63];
+// let arr_Tuple : [number , string, string, number] = [23, "sachin", "sharma", 63]
 // console.log(arr_Tuple)
 // Declaration and initialization
 var toupe1 = [23, "sachin"];
 //Push operation
 toupe1.push(2, "sharma");
-console.log("Length of touple is ", toupe1.length);
-console.log(toupe1);
+// console.log("Length of touple is ", toupe1.length)
+// console.log(toupe1)
 //POP operation 
-toupe1.pop();
-console.log("Length of touple is ", toupe1.length);
-console.log(toupe1);
+// toupe1.pop()
+// console.log("Length of touple is ", toupe1.length)
+// console.log(toupe1)
 // Update of modefy the touple value
-toupe1[0] = 55;
-console.log("First value is changed", toupe1[0]);
+// toupe1[0] = 55;
+// console.log("First value is changed",toupe1[0])
 // Clear touple field
-toupe1 = [0, ""];
-console.log("Empty touple is ", toupe1);
+// toupe1 = [0,""]
+// console.log("Empty touple is ", toupe1)
+// DeStructuring the touple
+var age = toupe1[0], name = toupe1[1];
+// console.log("Name is " ,name)
+// console.log("age is " ,age)
+// Passing the touple as parameter
+function printParaTouple(toupe_arr) {
+    console.log("Length of the touple is : ", toupe_arr.length);
+}
+printParaTouple(toupe1);

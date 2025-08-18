191d35cfc57c2e8c3d7b9287dce99ce833f7c7f7
SachinSharma22 <s830sharma@gmail.com>
2025-08-18 16:10:36 +0530

    push operation in touple

--- Basics/touple.js
+++ Basics/touple.js
@@ -1,2 +1,7 @@
 var arr_Tuple = [23, "sachin", "sharma", 63];
-console.log(arr_Tuple);
+// console.log(arr_Tuple)
+// Declaration and initialization
+var toupe1 = [23, "sachin"];
+//Push operation
+toupe1.push(2, "sharma");
+console.log(toupe1);
--- Basics/touple.ts
+++ Basics/touple.ts
@@ -1,2 +1,9 @@
 let arr_Tuple : [number , string, string, number] = [23, "sachin", "sharma", 63]
-console.log(arr_Tuple)
+// console.log(arr_Tuple)
+
+// Declaration and initialization
+
+let toupe1 : [number, string] = [23, "sachin"];
+ //Push operation
+toupe1.push(2,"sharma")
+console.log(toupe1)

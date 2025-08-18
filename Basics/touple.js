c4dac2f33582a5d10f32975ec087ec5792dac306
SachinSharma22 <s830sharma@gmail.com>
2025-08-18 16:14:55 +0530

    operate pop operationn on touple and also find its length with the help of length property

--- Basics/touple.js
+++ Basics/touple.js
@@ -4,4 +4,9 @@ var arr_Tuple = [23, "sachin", "sharma", 63];
 var toupe1 = [23, "sachin"];
 //Push operation
 toupe1.push(2, "sharma");
+console.log("Length of touple is ", toupe1.length);
+console.log(toupe1);
+//POP operation 
+toupe1.pop();
+console.log("Length of touple is ", toupe1.length);
 console.log(toupe1);
--- Basics/touple.ts
+++ Basics/touple.ts
@@ -6,4 +6,11 @@ let arr_Tuple : [number , string, string, number] = [23, "sachin", "sharma", 63]
 let toupe1 : [number, string] = [23, "sachin"];
  //Push operation
 toupe1.push(2,"sharma")
+console.log("Length of touple is ", toupe1.length)
 console.log(toupe1)
+
+//POP operation 
+toupe1.pop()
+console.log("Length of touple is ", toupe1.length)
+console.log(toupe1)
+

854ea8303e7e90eb8749906a6b1072775e991054
SachinSharma22 <s830sharma@gmail.com>
2025-08-18 15:52:27 +0530

    string enums a more descriptive the numerical enums

--- Basics/practice.js
+++ Basics/practice.js
@@ -1,3 +1,21 @@
 var num1;
 num1 = null;
 console.log(num1);
+// num1 = 20; Not assignable to null
+var carDetail;
+(function (carDetail) {
+    carDetail[carDetail["Tata"] = 10] = "Tata";
+    carDetail[carDetail["Volvo"] = 11] = "Volvo";
+    carDetail[carDetail["Mahindra"] = 12] = "Mahindra";
+})(carDetail || (carDetail = {}));
+// console.log(carDetail);
+// console.log(`The place of Volov is ${carDetail.Volvo}`)
+var fruits;
+(function (fruits) {
+    fruits["apple"] = "APPLE";
+    fruits["banana"] = "BANANA";
+    fruits["mango"] = "MANGO";
+    fruits["orange"] = "ORANGE";
+})(fruits || (fruits = {}));
+// console.log(fruits);
+// console.log("Fruit is: ",fruits.apple)
--- Basics/practice.ts
+++ Basics/practice.ts
@@ -3,3 +3,23 @@ num1 = null;
 console.log(num1)
 
 // num1 = 20; Not assignable to null
+
+enum carDetail  {
+    Tata = 10,
+    Volvo,
+    Mahindra
+}
+
+// console.log(carDetail);
+// console.log(`The place of Volov is ${carDetail.Volvo}`)
+
+enum fruits {
+    apple="APPLE",
+    banana="BANANA",
+    mango = "MANGO",
+    orange = "ORANGE"
+}
+
+// console.log(fruits);
+// console.log("Fruit is: ",fruits.apple)
+
